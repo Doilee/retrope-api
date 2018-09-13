81f212ccf90cc7f06005d23f6751a61fe45a0544
Matthijs <Matthijs0894@hotmail.com>
2018-09-13 16:45:47 +0200

    Change name in session to string

--- database/migrations/2018_11_08_070132_create_sessions_table.php
+++ database/migrations/2018_11_08_070132_create_sessions_table.php
@@ -15,7 +15,7 @@ class CreateSessionsTable extends Migration
         Schema::create('sessions', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('host_id')->unsigned();
-            $table->integer('name');
+            $table->string('name');
             $table->integer('timer')->default(180);
             $table->boolean('is_public')->default(true);
             $table->string('invitation_code');
