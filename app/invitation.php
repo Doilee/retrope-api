38bb564112b3f1f39a57e172a6a01e6dde87e583
Matthijs <Matthijs0894@hotmail.com>
2018-09-25 17:07:59 +0200

    Added Invitation model

---
+++ app/invitation.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property Session session
+ */
+class invitation extends Model
+{
+    protected $fillable = [
+        'send_at',
+        'scheduled_at'
+    ];
+
+    protected $dates = [
+        'send_at',
+        'scheduled_at'
+    ];
+
+    public function session()
+    {
+        return $this->belongsTo(Session::class);
+    }
+}
