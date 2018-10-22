3043be9471ba99f6df9d387039d7c965f4ffb66f
Matthijs <Matthijs0894@hotmail.com>
2018-10-22 19:39:34 +0200

    Added Client Model

---
+++ app/Client.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * App\Client
+ *
+ * @property int $id
+ * @property string $name
+ * @property \Illuminate\Support\Carbon $subscription_expires_at
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\User[] $users
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Client whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Client whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Client whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Client whereSubscriptionExpiresAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Client whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
+class Client extends Model
+{
+    protected $fillable =[
+        'name',
+        'subscription_expires_at'
+    ];
+
+    protected $dates = [
+        'subscription_expires_at'
+    ];
+
+    public function active() : bool
+    {
+        return $this->subscription_expires_at ? $this->subscription_expires_at->isPast() : false;
+    }
+
+    public function users()
+    {
+        return $this->hasMany(User::class);
+    }
+}
