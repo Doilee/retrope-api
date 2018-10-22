16e8a00afb662baca9e6085176ae2cdc184903e8
Matthijs <Matthijs0894@hotmail.com>
2018-10-22 19:43:03 +0200

    Added ClientController to manage clients as admin

---
+++ app/Http/Controllers/Admin/ClientController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Client;
+use App\Http\Controllers\Controller;
+use App\User;
+use Auth;
+use Illuminate\Http\Request;
+use Spatie\Permission\Middlewares\RoleMiddleware;
+
+class ClientController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Client::paginate(25);
+    }
+
+    /**
+     * Store a newly created client in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|string|min:2|max:255|unique:clients',
+            'subscription_expires_at' => 'required|datetime'
+        ]);
+
+        $data = $request->only(['name', 'subscription_expires_at']);
+
+        $client = new Client($data);
+
+        return response()->json([
+            'message' => 'Successfully made client',
+            'client' => $client
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Client $client
+     *
+     * @return Client
+     */
+    public function show(Client $client)
+    {
+        return $client;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Client  $client
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Client $client)
+    {
+        $this->validate($request, [
+            'name' => 'required|string|min:2|max:255',
+            'subscription_expires_at' => 'required|datetime'
+        ]);
+
+        $data = $request->only(['name', 'subscription_expires_at']);
+
+        $client->fill($data);
+
+        $client->save();
+
+        return response()->json([
+            'message' => 'Edit successful!',
+            'user' => $client
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Client  $client
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Client $client)
+    {
+        $client->delete();
+
+        return response()->json([
+            'message' => 'Successfully deleted client'
+        ])
+    }
+}
