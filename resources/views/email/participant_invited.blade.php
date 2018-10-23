c92a0f94ea488dc3c04dcf4e496182427626d037
Matthijs <Matthijs0894@hotmail.com>
2018-10-23 11:36:30 +0200

    Fixed non-existing method isGuest error

--- resources/views/email/participant_invited.blade.php
+++ resources/views/email/participant_invited.blade.php
@@ -9,11 +9,7 @@
     You've been invited to a retrospective of RETROPE.
     <br>
 
-    @if($invite && $invite->player->user->isGuest())
-        Click <a href="{{ env('FRONT_END_URL') . '/invite/' . $invite->token }}">here</a> to join!
-    @else
-        Click <a href="{{ env('FRONT_END_URL') }}">here</a> to join!
-    @endif
+    Click <a href="{{ env('FRONT_END_URL') }}">here</a> to join!
 </div>
 
 </body>
