afae54765c2ba0287ad875d96c7b08b46556dd86
Matthijs <Matthijs0894@hotmail.com>
2018-10-27 11:59:29 +0200

    Auto create client relation when building Subscription from factory

--- database/factories/SubscriptionFactory.php
+++ database/factories/SubscriptionFactory.php
@@ -4,6 +4,9 @@ use Faker\Generator as Faker;
 
 $factory->define(\App\Subscription::class, function (Faker $faker) {
     return [
+        'client_id' => function() {
+            return factory(\App\Client::class)->create()->id;
+         },
         'type' => $faker->randomElement(['trial', 'pro']),
         'expires_at' => now()->addYear()->toDateTimeString()
     ];
