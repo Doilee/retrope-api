9b0d76e0ea7d6a0ec75b63dbee540437c76864dd
Matthijs <Matthijs0894@hotmail.com>
2018-11-05 14:54:09 +0100

    Allow Origin *

--- config/cors.php
+++ config/cors.php
@@ -20,7 +20,7 @@ return [
         'allow_credentials' => false,
 
         'allow_origins' => [
-            env('FRONT_END_URL')
+            '*',
         ],
 
         'allow_methods' => [
@@ -35,6 +35,7 @@ return [
         'allow_headers' => [
             'Content-Type',
             // 'X-Auth-Token',
+            // 'X-Requested-With',
             'Origin',
             'Authorization',
         ],
